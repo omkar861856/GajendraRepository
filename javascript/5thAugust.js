f9e4d002fffe8185928cc3ca987d1bf9894a0906
Omkar <Omkar861856@gmail.com>
2024-08-05 21:53:46 +0530

    5th aug

---
+++ javascript/5thAugust.js
@@ -0,0 +1,40 @@
+// primitive types and reference types
+
+//primitive types
+
+// string ,numbers, booleans, null and undefined
+
+let a = "Hello World"
+let b = 2;
+let c = true;
+let d = null;
+let e;
+
+// reference types
+
+// objects, arrays and functions
+
+let f = {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    issues: [],//null array
+    pitfalls: undefined, 
+}
+
+let g = ["apple", 3, "cherry"]
+
+let h = function () {
+     return ("Hello from a function")
+}
+
+console.log(a, b, c, d, e, f, g, h())
+
+console.log(a +" "+ b) //concatenation with type coersion
+
+// type coercion
+
+
+
+
+
